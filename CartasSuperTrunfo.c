6b375c7a4ca1fb4d7eb99f86ac7e6d6e52aef828
Geovanni <145285671+gegecs@users.noreply.github.com>
2025-02-13 15:30:18 +0000

    PIB per capita e Densidade populacional adicionados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,10 @@
 
 int main() {
     //Nenhum valor será inserido aqui, pois o usuário irá inserir pelo scanf
-    //Variável para a quantidade da população
-    int populacao;
-    //Variável para a quantidade de pontos turísticos
-    int pontosTuristicos;
-    //Variável para o valor da área
-    float area;
-    //Variável para o valor do PIB
-    float pib;
+    //Variável para a quantidade da população e quantidade de pontos turísticos
+    int populacao, pontosTuristicos;
+    //Variável para o valor da área, valor do PIB, valor do PIB per capita e densidade populacional
+    float area, pib, pibCapita, densidade;
     //Variável char sem array, pois utilizaremos apenas a inicial do estado
     char estado;
     //Variável para o código da cidade
@@ -23,7 +19,6 @@ int main() {
     printf("Digite a inicial do estado:\n");
     //Utilizaremos o "%c" por conta do estado estar em um formato char de apenas um caractere.
     scanf("%c", &estado);
-
     
     printf("Digite o código da cidade:\n");
     //Transformaremos o código em uma string, por isso o "%s".
@@ -47,6 +42,11 @@ int main() {
     printf("Digite a quantidade de pontos turísticos:\n");
     scanf("%d", &pontosTuristicos);
     
+    //pibCapita recebendo o valor da divisão entre o pib e a população que o usuário digitou
+    pibCapita = pib / populacao;
+    //densidade recebendo o valor da divisão entre a população e a área que o usuário digitou
+    densidade = populacao / area;
+
     // Mensagem dizendo que a carta foi cadastrada.
     printf("\nA sua carta foi cadastrada!\n");
     // Mensagem informando a inicial do estado.
@@ -58,10 +58,14 @@ int main() {
     // Mensagem mostrando a quantidade de população.
     printf("População: %d\n", populacao);
     // Mensagem mostrando a área em km².
-    printf("Área: %f km²\n", area);
+    printf("Área: %.2f km²\n", area);
+    // Mensagem mostrando a densidade populacional.
+    printf("Densidade populacional: %.2f\n", densidade);
     // Mensagem mostrando o PIB da cidade.
-    printf("PIB: %f bilhões de reais\n", pib);
-    // Mensagem apresentado a quantidade de pontos turísticos da cidade
+    printf("PIB: %.3f bilhões de reais\n", pib);
+    // Mensagem mostrando o PIB per capita da cidade.
+    printf("PIB per capita: %.3f\n", pibCapita);
+    // Mensagem apresentado a quantidade de pontos turísticos da cidade.
     printf("Número de pontos turísticos: %d\n", pontosTuristicos);
 
     return 0;
